69abe94aea4f8f18f645d8553f5ce5cedcfefbf2
Pranay Anand <pranayanand123@gmail.com>
2017-10-24 20:39:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pranayanand/webapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.pranayanand.webapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.co.in");  // or url of website to be loaded in app
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+
+
+    }
+}
